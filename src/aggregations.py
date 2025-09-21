c9ce25715d4a953fb4ec8a68141c82216119c47d
Parth <112978352+parthhhhh12@users.noreply.github.com>
2025-09-21 20:05:43 +0530

    Update aggregations.py

--- src/aggregations.py
+++ src/aggregations.py
@@ -1,32 +1,20 @@
-# transformations.py
-from pyspark.sql.functions import col, to_timestamp
+# aggregations.py
+from pyspark.sql.functions import count, sum, date_format
 
-def clean_and_transform(raw_df):
+def aggregate_trips(df_clean):
     """
-    Cleans and transforms raw NYC Taxi data.
+    Aggregates trip data to calculate daily stats by VendorID.
     """
-    df_clean = (raw_df
-        .withColumn("VendorID", col("VendorID").cast("int"))
-        .withColumn("lpep_pickup_datetime", to_timestamp("lpep_pickup_datetime", "yyyy-MM-dd HH:mm:ss"))
-        .withColumn("lpep_dropoff_datetime", to_timestamp("lpep_dropoff_datetime", "yyyy-MM-dd HH:mm:ss"))
-        .withColumn("RatecodeID", col("RatecodeID").cast("int"))
-        .withColumn("PULocationID", col("PULocationID").cast("int"))
-        .withColumn("DOLocationID", col("DOLocationID").cast("int"))
-        .withColumn("passenger_count", col("passenger_count").cast("int"))
-        .withColumn("trip_distance", col("trip_distance").cast("double"))
-        .withColumn("fare_amount", col("fare_amount").cast("double"))
-        .withColumn("extra", col("extra").cast("double"))
-        .withColumn("mta_tax", col("mta_tax").cast("double"))
-        .withColumn("tip_amount", col("tip_amount").cast("double"))
-        .withColumn("tolls_amount", col("tolls_amount").cast("double"))
-        .withColumn("ehail_fee", col("ehail_fee").cast("double"))
-        .withColumn("improvement_surcharge", col("improvement_surcharge").cast("double"))
-        .withColumn("total_amount", col("total_amount").cast("double"))
-        .withColumn("payment_type", col("payment_type").cast("int"))
-        .withColumn("trip_type", col("trip_type").cast("int"))
-        .withColumn("congestion_surcharge", col("congestion_surcharge").cast("double"))
-        .filter(col("passenger_count") > 0)
-        .filter(col("total_amount") > 0)
-        .filter(col("trip_distance") > 0)
+    agg_df = (df_clean
+        .withColumn("pickup_date", date_format("lpep_pickup_datetime", "yyyy-MM-dd"))
+        .groupBy("VendorID", "pickup_date")
+        .agg(
+            count("*").alias("total_trips"),
+            sum("fare_amount").alias("total_fare"),
+            sum("tip_amount").alias("total_tips"),
+            sum("total_amount").alias("total_revenue")
+        )
     )
-    return df_clean
+    return agg_df
+
+
